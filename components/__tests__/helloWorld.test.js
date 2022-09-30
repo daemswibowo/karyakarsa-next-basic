d67f303e8bb412c43e0776a778c236fa7185bbcb
Dimas Wibowo <dimas@karyakarsa.com>
2022-09-30 13:51:17 +0700

    add selector comment on helloworld.test.js

--- components/__tests__/helloWorld.test.js
+++ components/__tests__/helloWorld.test.js
@@ -5,7 +5,7 @@ describe("HelloWorld component test", () => {
   it("should displaying hello world", async () => {
     render(<HelloWorld />); // panggil component yang akan ditest
 
-    const greeting = await screen.findByTestId("greetings"); // get object dalam component HelloWorld
+    const greeting = await screen.findByTestId("greetings"); // get object selector dalam component HelloWorld
 
     expect(greeting).toHaveTextContent("Hello world!"); // expect si component HelloWorld punya text "Hello world!"
   });
